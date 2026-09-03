70633429bf389583394d76d24d9e1682ff341feb
kbrown2311 <kbrown351@boyslatin.net>
2026-09-03 12:49:54 +0000

    work

---
+++ unit 1/variableActivity.py
@@ -0,0 +1,47 @@
+# variable- A container that stores data. you can name a variable anything you want, so long as it is descriptive.
+
+name= "kaden brown"
+
+# VARIABLES - a container that stores data. 
+# you can  name your container almost anything  
+# you want, so long as it is descriptive. 
+
+# Variables operate on the priniciple of Key / Value pairs
+
+user = 'Ian' # a variable storing a string
+yearsOfExperience = 10 # a variable storing a number
+graduated = True # a variable storing a boolean
+
+Ja3SneakersCount = 9
+Ja3SneakersPrice = 150.00
+Ja3SneakersSKU = "91022382913"
+
+Douglas42="" 
+S21average="" 
+
+
+# When we want to create a variable, we make up a word, 
+# use the equal sign and then assign it a data type.  
+# whenever the computer sees our variable word it will
+# alway use the data inside. 
+
+
+"count apples in a basket" = 10
+"count students" = 25
+"insurance trip" = True
+"broilvergold" = True
+"drink price 3" = 5.99
+
+ # Variables naming rules 
+# 1. Variables must start with a letter or underscore
+# 2. Variables cannot contain spaces
+# 3. Variables are case-sensitive
+# 4. Variables cannot be reserved keywords
+
+# VARIABLES naming conventions
+# 1. camelCase - first word is lowercase, subsequent words are capitalized the words are like a camel's humps.
+
+# 2. PascalCase - first letter of each word is capitalized and underscores are not used.
+
+# 3. snake_case - words are separated by underscores 
+#the reason for this is to make it easier to read and understand the variable name.
