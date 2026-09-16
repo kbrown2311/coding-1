74a05352398633cdaf80cb3fff015ed6565260b3
kbrown2311 <kbrown351@boyslatin.net>
2026-09-16 12:55:58 +0000

    work

--- unit 1/OperatorsActivity.py
+++ unit 1/OperatorsActivity.py
@@ -11,3 +11,29 @@ print( 15 > 18)
 print( "Coding 1" == "coding1")
 
 print("0" != 0)
+
+book = 10.99
+tablet = 399.99
+print(book + tablet)
+410.98
+
+
+tax = 0.07
+shoes = 200.00
+print(shoes * tax)
+14.000000000000002
+
+
+cart = 98.87
+amountToGetDiscount = 100.00
+print(cart >= amountToGetDiscount) 
+
+
+GPA = 86 
+Recommendation = true
+print(GPA > 85 and Recommendation == true)
+
+
+parentConactA= false
+parentContactB= true
+print(parentConactA or parentContactB)
