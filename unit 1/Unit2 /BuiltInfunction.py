fa65df1ee494c1975cd547a9672aff73572df33d
kbrown2311 <kbrown351@boyslatin.net>
2026-09-22 12:53:52 +0000

    Work

---
+++ unit 1/Unit2 /BuiltInfunction.py
@@ -0,0 +1,13 @@
+# The print() function is used to display output to the console. It can take multiple arguments and can format the output in various ways.
+# Data casting functions 
+# these are built in (pre written) functions 
+#that are used to convert data from one type to another.
+ year = 1738
+ year_now = 2026
+# print( year_now + year)  
+
+# int() function is used to convert a value to an integer data type. 
+# It can take a string or a float as an argument and return the same integer value. 
+price = 134.98  
+print (int("134.98"))
+print (int(price))  
