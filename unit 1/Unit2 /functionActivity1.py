3cc52d607d26728d9d04b8b3b6b61d868202902a
kbrown2311 <kbrown351@boyslatin.net>
2026-09-24 12:55:16 +0000

    work

--- unit 1/Unit2 /functionActivity1.py
+++ unit 1/Unit2 /functionActivity1.py
@@ -1,6 +1,16 @@
 # num1 = int(input("please type a number: "))
 # print(num1 > 85)
+
 # if it is bigger than 85 it will return True, 
 # if not it will return False
 
-print(int(input("360 needs to equal 500: ")))
+# 2.
+# taking in a number with input
+# we are adding to 360
+# and it should print out 500
+# num2 = int(input("140"))
+# print(360 + num2)
+
+# 3.
+val= input()
+print(val == "Boys Latin")
